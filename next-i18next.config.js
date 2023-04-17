f9d81bd94e6aa09ea310853d35450bd5655a2d9d
inra0 <32350438+inra0@users.noreply.github.com>
2023-04-17 21:33:58 +0700

    Add files via upload

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    defaultLocale: "en",
+    defaultLocale: 'en',
     locales: [
       "bn",
       "de",
@@ -9,10 +9,13 @@ module.exports = {
       "fr",
       "he",
       "id",
+      "it",
       "ja",
       "ko",
+      "pl",
       "pt",
       "ru",
+      "ro",      
       "sv",
       "te",
       "vi",
